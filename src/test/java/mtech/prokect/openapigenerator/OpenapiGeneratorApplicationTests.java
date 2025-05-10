9b1d8c4594a0f24f0fbeb421095d0ea7d64abd8c
muradshahin <shahinmurad43@gmail.com>
2025-05-10 19:29:16 +0300

    first commit

---
+++ src/test/java/mtech/prokect/openapigenerator/OpenapiGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package mtech.prokect.openapigenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenapiGeneratorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
