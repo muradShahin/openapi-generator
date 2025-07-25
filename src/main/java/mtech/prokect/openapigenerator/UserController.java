d053cee1ace8b9060535210b808d2f6104f4be69
muradshahin <shahinmurad43@gmail.com>
2025-07-25 20:22:14 +0300

    SP3 release related changes

--- src/main/java/mtech/prokect/openapigenerator/UserController.java
+++ src/main/java/mtech/prokect/openapigenerator/UserController.java
@@ -7,12 +7,16 @@ import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
+
+import org.hibernate.validator.internal.util.logging.Log_.logger;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
+
+import ch.qos.logback.classic.Logger;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -41,6 +45,15 @@ public class UserController {
             @NotNull @Pattern(regexp = "^\\+962[1-9][0-9]{8}$") @Size(max = 10) @Parameter(name = "phoneNumber", description = "Phone number in Jordanian format starting with +962 followed by 9 digits", required = true, in = ParameterIn.QUERY) @Valid @RequestParam(value = "phoneNumber", required = true) String phoneNumber,
             @NotNull @Pattern(regexp = "^[A-Za-z\\\\s]{1,60}$") @Size(max = 60) @Parameter(name = "name", description = "Name should be alphabetic only and max 60 characters", required = true, in = ParameterIn.QUERY) @Valid @RequestParam(value = "name", required = true) String name
     ){
+
+        Logger logger = (Logger) org.slf4j.LoggerFactory.getLogger(UserController.class);
+        logger.info("Received request with phoneNumber: {} and name: {}", phoneNumber, name);
+        // Simulate some processing
+        try {
+            Thread.sleep(1000); // Simulate processing delay
+        } catch (InterruptedException e) {
+            logger.error("Error occurred while processing request", e);
+        }
 
         return  new ResponseEntity<>(phoneNumber,HttpStatus.OK);
     }
